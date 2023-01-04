cb48517abde811adf1c5648225f0383bdfdbabbd
martingerardoc <martingerardoc56@gmail.com>
2023-01-03 21:15:28 -0300

    Fixed problems where nothing was displayed on the screen.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("We are going to make some desitions");
+        Console.WriteLine("Making some changes on the file");
     }
 }
