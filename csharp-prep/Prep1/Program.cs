3508ca83e5d5abe40922a188ca6ee8284038d356
martingerardoc <martingerardoc56@gmail.com>
2023-01-07 10:39:48 -0300

    Finished prep 1 and saving changges

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Console.WriteLine("Hello Prep1 World!");
+        // We ask the user his name and his last name and showed on terminal.
+        Console.WriteLine("what is your name?");
+        String name=Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        String LastName=Console.ReadLine();
+        Console.WriteLine($"your name is {LastName}, {name} {LastName}");
     }
 }
