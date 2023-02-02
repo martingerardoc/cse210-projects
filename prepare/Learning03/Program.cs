efe99c2e9762ad5617959d31e8599e7198c64461
martingerardoc <martingerardoc56@gmail.com>
2023-02-01 22:16:53 -0300

    starting with encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,147 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
 
-        string input;
-        do
-        {
-        Console.WriteLine("Welcome to the Journal Program!");
-        Console.WriteLine("Please select one of the folowing choices:");
-        Console.WriteLine("1. Write.");
-        Console.WriteLine("2. Display.");
-        Console.WriteLine("3. Load.");
-        Console.WriteLine("4. Save.");
-        Console.WriteLine("5. Quit.");
-        input =Console.ReadLine();
-        if(input=="1"){
-          Console.WriteLine("your choice was 1");
-          Write();
-          Console.ReadLine();
-        }
-        else if (input=="2"){
-          Console.WriteLine("your choice was 2");
-          Console.ReadLine();
-        }
-        else if (input=="3"){
-          Console.WriteLine("your choice was 3");
-          Console.ReadLine();
-        }
-        else if (input=="4"){
-          Console.WriteLine("your choice was 4");
-          Console.ReadLine();
-        }
-        }
-      while (input!="5");
-      Console.WriteLine("Thanks for using the Journal Program.");
-      Console.ReadLine();
-    }
-     static void Write() 
-    {
-      Console.WriteLine("please enter your text");
-      Console.ReadLine();
-      string writeText = "Hello World!";  // Create a text string
-      File.WriteAllText("filename.txt", writeText);  // Create a file and write the contents of writeText to it
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
 
-      string readText = File.ReadAllText("filename.txt"); // Read the contents of the file
-      Console.WriteLine(readText); 
-      // Output the content
-      // Create fille//
-      TextWriter file = new StreamWriter("cs210.txt");
-      Console.WriteLine("Please enter your text");
-      string message=Console.ReadLine().ToString();
-      file.WriteLine(message);
-      file.Close();
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
 
-      //reading file
-      TextReader read_File= new StreamReader("cs210.txt");
-      Console.WriteLine(read_File.ReadToEnd());
-      read_File.Close();
-
-      //Insert new tex to the file
-      TextWriter file2 = File.AppendText("cs210.txt");
-      //Console.WriteLine("inser another text to add to the file");
-      Console.WriteLine("We are going to add the date");
-      DateTime theCurrentTime = DateTime.Now;
-      string dateText = theCurrentTime.ToShortDateString();
-      //string text2= Console.ReadLine().ToString();
-      file2.WriteLine(dateText);
-      file2.Close();
-
-      // Another reading file
-
-      TextReader read_file2= new StreamReader("cs210.txt");
-      Console.WriteLine(read_file2.ReadToEnd());
-      read_file2.Close();
-
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
-// Don't forget to put this at the top, so C# knows where to find the StreamWriter class
-/*using System;
-using System.IO;  // include the System.IO namespace
-
-namespace MyApplication
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      
-      /*string writeText = "Hello World!";  // Create a text string
-      File.WriteAllText("filename.txt", writeText);  // Create a file and write the contents of writeText to it
-
-      string readText = File.ReadAllText("filename.txt"); // Read the contents of the file
-      Console.WriteLine(readText); // Output the content*/
-      // Create fille//
-      /*TextWriter file = new StreamWriter("cs210.txt");
-      Console.WriteLine("Please enter your text");
-      string message=Console.ReadLine().ToString();
-      file.WriteLine(message);
-      file.Close();
-
-      //readin file
-      TextReader read_File= new StreamReader("cs210.txt");
-      Console.WriteLine(read_File.ReadToEnd());
-      read_File.Close();
-
-      //Insert new tex to the file
-      TextWriter file2 = File.AppendText("cs210.txt");
-      //Console.WriteLine("inser another text to add to the file");
-      Console.WriteLine("We are going to add the date");
-      DateTime theCurrentTime = DateTime.Now;
-      string dateText = theCurrentTime.ToShortDateString();
-      //string text2= Console.ReadLine().ToString();
-      file2.WriteLine(dateText);
-      file2.Close();
-
-      // Another reading file
-
-      TextReader read_file2= new StreamReader("cs210.txt");
-      Console.WriteLine(read_file2.ReadToEnd());
-      read_file2.Close();
-
-
-    }
-  }
-}
-*/
-
-/*using System;
-namespace MyAplication{
-  class Program{
-    static void Main(string[] args)
-    {
-
-      Console.WriteLine("Hello World");
-    }
-    static void DateTime(){
-      Console.WriteLine("This is the date");
-      DateTime theCurrentTime = DateTime.Now;
-      string dateText = theCurrentTime.ToShortDateString();
-      //string text2= Console.ReadLine().ToString();
-      Console.WriteLine(dateText);
-
-    }
-  }
-}
-*/
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine("We are going to do some examples");
+        
     }
 }
