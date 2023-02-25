9639ba0997d80a41efcb2435c75dbe5cd0716d73
martingerardoc <martingerardoc56@gmail.com>
2023-02-25 17:14:00 -0300

    first part assigment week 08

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,21 @@
+﻿namespace cse210_projects;
+public class Activity
+{
+    private string name;
+    private int age;
+    private string gender;
+    private int time;
+
+    public string Name { get => name; set => name = value; }
+    public int Age { get => age; set => age = value; }
+    public string Gender { get => gender; set => gender = value; }
+    public int Time { get => time; set => time = value; }
+
+    public void getActivity(string _name, int _age, string _gender, int _time){
+        name = _name;
+        this.age=_age;
+        gender=_gender;
+        time=_time;
+
+    }
+}
